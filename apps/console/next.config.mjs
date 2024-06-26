7bd133a9e565ffe0e8f399770fca5e9f2029343d
giangndm <45644921+giangndm@users.noreply.github.com>
2024-06-26 22:44:07 +0700

    fix: build static page reload 404 error

--- apps/console/next.config.mjs
+++ apps/console/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
   transpilePackages: ['@packages/ui', '@packages/core', '@packages/react-hooks', '@packages/react-ui'],
 }
 
